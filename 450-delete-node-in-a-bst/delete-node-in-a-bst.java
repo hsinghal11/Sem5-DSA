b613051c0de4d1e903b4dd3954ebc052774e9d99
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-08-07 11:46:25 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (76.60%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+        public TreeNode deleteNode(TreeNode root, int key) {
+            if( root == null){
+                return null;
+            }
+            if(root.val < key){
+                root.right = deleteNode(root.right, key);
+            }else if(root.val > key){
+                root.left = deleteNode(root.left, key);
+            }else{
+                // 1 or 0 child
+                if(root.right == null){
+                    return root.left;
+                }else if(root.left == null){
+                    return root.right;
+                }else{
+                    int max = max(root.left);
+                    root.left = deleteNode(root.left, max);
+                    root.val = max;
+                }
+            }
+            return root;
+        }
+
+        // left ke max ke liye 
+        public int max (TreeNode root){
+            if(root == null){
+                return Integer.MIN_VALUE;
+            }
+            int max = max(root.right);
+            return Math.max(max, root.val);
+        }
+
+        // right ke min ke liye 
+        public int min(TreeNode root){
+            if(root == null){
+                return Integer.MAX_VALUE;
+            }
+            int min = min(root.left);
+            return Math.max(min, root.val);
+        }
+    }
