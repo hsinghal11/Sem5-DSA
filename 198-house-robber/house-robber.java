536385b28e739e1566ef3e1e977ef55951a56641
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-09-03 11:35:04 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (60.37%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,17 +2,17 @@ class Solution {
     public int rob(int[] arr) {
         int[] dp = new int[arr.length];
         Arrays.fill(dp, -1);
-        return Robber(arr, 0, dp);
+        return Robber(arr, arr.length-1, dp);
     }
     public int Robber(int[] arr, int i, int[] dp){
-        if(i >= arr.length){
+        if(i < 0){
             return 0;
         }
         if(dp[i] != -1){
             return dp[i];
         }
-        int rob = arr[i] + Robber(arr, i+2, dp);
-        int dont_rob = Robber(arr, i+1, dp);
+        int rob = arr[i] + Robber(arr, i-2, dp);
+        int dont_rob = Robber(arr, i-1, dp);
         return dp[i] = Math.max(rob, dont_rob);
     }
 }
