53d9504283472497330dd7deca7b1c155ddaa2f2
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-09-21 21:39:50 +0530

    Time: 13 ms (71.79%) | Memory: 44.8 MB (73.52%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,38 +1,68 @@
 class Solution {
-    public String minWindow(String s, String t) {
-        if (s.length() < t.length()) return "";
+    private boolean isValid(String s, String t, int mid, int[] start) {
+        int[] need = new int[128];
+        int distinct = 0;
 
-        int[] need = new int[128];  
         for (char c : t.toCharArray()) {
+            if (need[c] == 0) distinct++;
             need[c]++;
         }
 
-        int left = 0, right = 0;
-        int required = t.length();  
-        int minLen = Integer.MAX_VALUE;
-        int start = 0;
+        int[] window = new int[128];
+        int currCount = 0;
 
-        while (right < s.length()) {
-            char c = s.charAt(right);
-            if(need[c] > 0){
-                required--;
-            }
-            need[c]--;
-            right++;
-            while(required == 0){
-                int len = right - left;
-                if(len < minLen){
-                    minLen = len;
-                    start = left;
-                }
-                char lc = s.charAt(left);
-                need[lc]++;
-                if(need[lc] > 0){
-                    required++;
-                }
-                left++;
+        // First window of size mid
+        for (int i = 0; i < mid && i < s.length(); i++) {
+            char c = s.charAt(i);
+            window[c]++;
+            if (window[c] == need[c]) currCount++;
+        }
+        if (currCount == distinct) {
+            start[0] = 0;
+            return true;
+        }
+
+        // Slide window across s
+        for (int i = mid; i < s.length(); i++) {
+            char add = s.charAt(i);
+            char remove = s.charAt(i - mid);
+
+            window[add]++;
+            if (window[add] == need[add]) currCount++;
+
+            if (window[remove] == need[remove]) currCount--;
+            window[remove]--;
+
+            if (currCount == distinct) {
+                start[0] = i - mid + 1;
+                return true;
             }
         }
-        return minLen == Integer.MAX_VALUE? "": s.substring(start, start+minLen);
+        return false;
+    }
+
+    public String minWindow(String s, String t) {
+        int n = s.length(), m = t.length();
+        if (m > n) return "";
+
+        int low = m, high = n;
+        int minLen = Integer.MAX_VALUE;
+        int[] start = new int[1];
+        String ans = "";
+
+        // Binary search over possible window size
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (isValid(s, t, mid, start)) {
+                if (mid < minLen) {
+                    minLen = mid;
+                    ans = s.substring(start[0], start[0] + mid);
+                }
+                high = mid - 1; // try smaller
+            } else {
+                low = mid + 1; // need bigger
+            }
+        }
+        return ans;
     }
 }
