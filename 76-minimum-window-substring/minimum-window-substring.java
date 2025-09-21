c743d06b491f860472061bc9d4ee8a3d5ab985f3
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-09-21 21:37:35 +0530

    Time: 2 ms (99.85%) | Memory: 44.8 MB (80.89%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -2,40 +2,37 @@ class Solution {
     public String minWindow(String s, String t) {
         if (s.length() < t.length()) return "";
 
-        int[] need = new int[128];   // freq of characters required
+        int[] need = new int[128];  
         for (char c : t.toCharArray()) {
             need[c]++;
         }
 
         int left = 0, right = 0;
-        int required = t.length();   // total chars we still need
+        int required = t.length();  
         int minLen = Integer.MAX_VALUE;
         int start = 0;
 
         while (right < s.length()) {
-            char rc = s.charAt(right);
-            if (need[rc] > 0) {
-                required--; // satisfied one needed char
+            char c = s.charAt(right);
+            if(need[c] > 0){
+                required--;
             }
-            need[rc]--;  // include char in window
+            need[c]--;
             right++;
-
-            // when all required chars are in current window
-            while (required == 0) {
-                if (right - left < minLen) {
-                    minLen = right - left;
+            while(required == 0){
+                int len = right - left;
+                if(len < minLen){
+                    minLen = len;
                     start = left;
                 }
-
                 char lc = s.charAt(left);
-                need[lc]++; // remove left char
-                if (need[lc] > 0) {
-                    required++; // we lost a needed char
+                need[lc]++;
+                if(need[lc] > 0){
+                    required++;
                 }
                 left++;
             }
         }
-
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+        return minLen == Integer.MAX_VALUE? "": s.substring(start, start+minLen);
     }
 }
