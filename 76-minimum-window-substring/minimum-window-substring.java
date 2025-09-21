a71263c4675b374baa796a021d237726cacd1b3d
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-09-21 21:28:52 +0530

    Time: 3 ms (88.05%) | Memory: 44.8 MB (80.89%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,65 +1,41 @@
-import java.util.*;
-
 class Solution {
     public String minWindow(String s, String t) {
-        int n = s.length();
-        int m = t.length();
-        if (m > n) return "";
+        if (s.length() < t.length()) return "";
 
-        int[] point = new int[1]; // to store start index of valid window
-        String ans = "";
-        int low = m, high = n;
-        int minLength = Integer.MAX_VALUE;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (isValid(s, t, point, mid)) {
-                // store current best
-                if (mid < minLength) {
-                    minLength = mid;
-                    ans = s.substring(point[0], point[0] + mid);
-                }
-                high = mid - 1; // try smaller window
-            } else {
-                low = mid + 1; // need bigger window
-            }
-        }
-        return ans;
-    }
-
-    public boolean isValid(String s, String t, int[] point, int mid) {
-        int[] freq = new int[256];
+        int[] need = new int[128];   // freq of characters required
         for (char c : t.toCharArray()) {
-            freq[c]++;
+            need[c]++;
         }
 
-        int[] freq2 = new int[256];
-        // First window of size mid
-        for (int i = 0; i < mid; i++) {
-            freq2[s.charAt(i)]++;
-        }
-        if (covers(freq2, freq)) {
-            point[0] = 0;
-            return true;
-        }
+        int left = 0, right = 0;
+        int required = t.length();   // total chars we still need
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
 
-        // Slide the window
-        for (int i = mid; i < s.length(); i++) {
-            freq2[s.charAt(i)]++;           // add new char
-            freq2[s.charAt(i - mid)]--;     // remove old char
-            if (covers(freq2, freq)) {
-                point[0] = i - mid + 1;
-                return true;
+        while (right < s.length()) {
+            char rc = s.charAt(right);
+            if (need[rc] > 0) {
+                required--; // satisfied one needed char
+            }
+            need[rc]--;  // include char in window
+            right++;
+
+            // when all required chars are in current window
+            while (required == 0) {
+                if (right - left < minLen) {
+                    minLen = right - left;
+                    start = left;
+                }
+
+                char lc = s.charAt(left);
+                need[lc]++; // remove left char
+                if (need[lc] > 0) {
+                    required++; // we lost a needed char
+                }
+                left++;
             }
         }
-        return false;
-    }
 
-    // helper to check if freq2 covers all required chars in freq
-    private boolean covers(int[] freq2, int[] freq) {
-        for (int j = 0; j < 256; j++) {
-            if (freq2[j] < freq[j]) return false;
-        }
-        return true;
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
     }
 }
