8177279997145dcbe0e9d3b080bca5f30c2ee941
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-09-21 21:26:16 +0530

    Time: 122 ms (10.65%) | Memory: 44.6 MB (93.68%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,46 +1,53 @@
+import java.util.*;
+
 class Solution {
     public String minWindow(String s, String t) {
         int n = s.length();
         int m = t.length();
-        if(m > n) return "";
+        if (m > n) return "";
 
-        int[] point = new int[1];
-        int low = m;
+        int[] point = new int[1]; // to store start index of valid window
         String ans = "";
-        int high = n;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(isValid(s,t,point,mid)){
-                ans = s.substring(point[0], point[0]+mid);
-                high = mid -1;
-            }else{
-                low = mid+1;
+        int low = m, high = n;
+        int minLength = Integer.MAX_VALUE;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (isValid(s, t, point, mid)) {
+                // store current best
+                if (mid < minLength) {
+                    minLength = mid;
+                    ans = s.substring(point[0], point[0] + mid);
+                }
+                high = mid - 1; // try smaller window
+            } else {
+                low = mid + 1; // need bigger window
             }
         }
         return ans;
     }
 
-    public boolean isValid(String s, String t, int[] point, int mid){
+    public boolean isValid(String s, String t, int[] point, int mid) {
         int[] freq = new int[256];
-        for(char c : t.toCharArray()){
+        for (char c : t.toCharArray()) {
             freq[c]++;
         }
 
         int[] freq2 = new int[256];
-        // First window
-        for(int i = 0; i < mid; i++){
+        // First window of size mid
+        for (int i = 0; i < mid; i++) {
             freq2[s.charAt(i)]++;
         }
-        if(covers(freq2, freq)){
+        if (covers(freq2, freq)) {
             point[0] = 0;
             return true;
         }
 
-        // Slide window
-        for(int i = mid; i < s.length(); i++){
+        // Slide the window
+        for (int i = mid; i < s.length(); i++) {
             freq2[s.charAt(i)]++;           // add new char
-            freq2[s.charAt(i-mid)]--;       // remove old char
-            if(covers(freq2, freq)){
+            freq2[s.charAt(i - mid)]--;     // remove old char
+            if (covers(freq2, freq)) {
                 point[0] = i - mid + 1;
                 return true;
             }
@@ -48,10 +55,10 @@ class Solution {
         return false;
     }
 
-    // Helper: checks if window covers t
-    private boolean covers(int[] freq2, int[] freq){
-        for(int j = 0; j < 256; j++){
-            if(freq2[j] < freq[j]) return false;
+    // helper to check if freq2 covers all required chars in freq
+    private boolean covers(int[] freq2, int[] freq) {
+        for (int j = 0; j < 256; j++) {
+            if (freq2[j] < freq[j]) return false;
         }
         return true;
     }
