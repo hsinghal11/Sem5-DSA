d45395724a06219287e79ae00e5239c2019a151f
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-09-24 00:30:51 +0530

    Time: 7 ms (95.00%) | Memory: 41.9 MB (77.50%) - LeetSync

--- 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.java
+++ 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.java
@@ -1,38 +1,69 @@
 class Solution {
     public long findKthSmallest(int[] coins, int k) {
-        long l = 0;
-        long r = (long)1e12;
+        // Step 1: Remove redundant coins
+        Arrays.sort(coins);
+        boolean[] redundant = new boolean[coins.length];
+        for (int i = 0; i < coins.length; i++) {
+            if (redundant[i]) continue;
+            for (int j = i + 1; j < coins.length; j++) {
+                if (!redundant[j] && coins[j] % coins[i] == 0) {
+                    redundant[j] = true;
+                }
+            }
+        }
+        List<Integer> filtered = new ArrayList<>();
+        for (int i = 0; i < coins.length; i++) {
+            if (!redundant[i]) filtered.add(coins[i]);
+        }
+        coins = filtered.stream().mapToInt(Integer::intValue).toArray();
+
+        int n = coins.length;
+        int allones = (1 << n);
+
+        // Step 2: Precompute all LCMs
+        long[] lcmArr = new long[allones];
+        lcmArr[0] = 1;
+        for (int mask = 1; mask < allones; mask++) {
+            int bit = Integer.numberOfTrailingZeros(mask);
+            int prev = mask ^ (1 << bit);
+            lcmArr[mask] = calculateLCM(lcmArr[prev], coins[bit]);
+        }
+
+        // Step 3: Binary search with tighter bounds
+        long l = k;
+        long r = (long) k * coins[0];
         long ans = 0;
-        while(l<=r){
-            long m = l + (r-l)/2;
-            long c = count(coins, m);
-            if(c < k){
-                l = m +1;
-            }else{
+
+        while (l <= r) {
+            long m = l + (r - l) / 2;
+            long c = count(lcmArr, m);
+
+            if (c < k) {
+                l = m + 1;
+            } else {
                 ans = m;
-                r = m-1;
+                r = m - 1;
             }
         }
         return ans;
     }
-    public long count(int[] coins, long m){
-        int n = coins.length;
-        int allones = (1<<n)-1;
+
+    // count using precomputed lcmArr
+    public long count(long[] lcmArr, long m) {
         long cnt = 0;
-        for(int i=1; i<=allones; i++){
-            long Lcm = 1;
-            for(int j=0; j<n; j++){
-                if((i & (1<<j)) > 0)
-                    Lcm = calculateLCM(Lcm, coins[j]);
-            }
-            if( (Long.bitCount(i) & 1) > 0){
-                cnt += m/Lcm;
-            }else{
-                cnt -= m/Lcm;
+        for (int mask = 1; mask < lcmArr.length; mask++) {
+            long Lcm = lcmArr[mask];
+            if (Lcm == 0 || Lcm > m) continue;
+
+            if ((Integer.bitCount(mask) & 1) == 1) {
+                cnt += m / Lcm;
+            } else {
+                cnt -= m / Lcm;
             }
         }
         return cnt;
     }
+
     public long calculateLCM(long a, long b) {
         if (a == 0 || b == 0) return 0;
         return (a / gcd(a, b)) * b;
