5cf1750f27d8f6ec1610b9eb045e79eaa6e03861
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-09-26 15:32:26 +0530

    Time: 2 ms (42.85%) | Memory: 57 MB (77.00%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,5 +1,6 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
+        /* extra space
         int[] f = new int[nums.length+1];
         for(int i=0; i<nums.length; i++){
             if(nums[i] <= nums.length && nums[i] > 0){
@@ -11,5 +12,25 @@ class Solution {
             if(ans == i && f[i] != 0) ans++;
         }
         return ans;
+        */
+
+        /** optimized solution */
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            while(nums[i] >0 && nums[i] <n && nums[i] != nums[nums[i]-1]){
+                swap(nums,i, nums[i]-1);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i] != i+1){
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
