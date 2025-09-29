8b2f462a796b506dcdb26a15e932b2b24de2a682
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-09-29 20:59:30 +0530

    Time: 3 ms (84.37%) | Memory: 49.5 MB (73.02%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,11 +1,10 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int ans = 0;
-        boolean[][] isVisited = new boolean[grid.length][grid[0].length];
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if(grid[i][j] == '1' && !isVisited[i][j]){
-                    solve(grid, i, j, isVisited);
+                if(grid[i][j] == '1'){
+                    solve(grid, i, j);
                     ans++;
                 }
             }
@@ -13,14 +12,14 @@ class Solution {
         return ans;
     }
 
-    private static void solve(char[][] grid, int i, int j, boolean[][] isVisited) {
-        if(i <0 || i> grid.length-1 || j<0 || j>grid[0].length-1 || isVisited[i][j] || grid[i][j] != '1'){
+    private static void solve(char[][] grid, int i, int j) {
+        if(i <0 || i> grid.length-1 || j<0 || j>grid[0].length-1 || grid[i][j] != '1'){
             return;
         }
-        isVisited[i][j] = true;
-        solve(grid, i+1, j, isVisited);
-        solve(grid, i, j+1, isVisited);
-        solve(grid, i-1, j, isVisited);
-        solve(grid, i, j-1, isVisited);
+        grid[i][j] = '0';
+        solve(grid, i+1, j);
+        solve(grid, i, j+1);
+        solve(grid, i-1, j);
+        solve(grid, i, j-1);
     }
 }
