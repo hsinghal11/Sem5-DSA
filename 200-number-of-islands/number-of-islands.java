f149ae9496611ec6697573b5f7b46d6402d8d84b
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-09-29 20:58:13 +0530

    Time: 4 ms (49.83%) | Memory: 50.2 MB (28.83%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -14,24 +14,13 @@ class Solution {
     }
 
     private static void solve(char[][] grid, int i, int j, boolean[][] isVisited) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{i, j});
-        isVisited[i][j] = true;
-        int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
-        while (!q.isEmpty()){
-            int[] cur = q.poll();
-            int curr = cur[0];
-            int curc = cur[1];
-            for(int[] dir: directions){
-                int newRow = curr + dir[0];
-                int newCol = curc + dir[1];
-                if (newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length) {
-                    if (grid[newRow][newCol] == '1' && !isVisited[newRow][newCol]) {
-                        isVisited[newRow][newCol] = true;
-                        q.add(new int[]{newRow, newCol});
-                    }
-                }
-            }
+        if(i <0 || i> grid.length-1 || j<0 || j>grid[0].length-1 || isVisited[i][j] || grid[i][j] != '1'){
+            return;
         }
+        isVisited[i][j] = true;
+        solve(grid, i+1, j, isVisited);
+        solve(grid, i, j+1, isVisited);
+        solve(grid, i-1, j, isVisited);
+        solve(grid, i, j-1, isVisited);
     }
 }
