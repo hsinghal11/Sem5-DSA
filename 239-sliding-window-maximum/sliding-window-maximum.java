2252ab70a498f98b33f0f5301d8504eebd2929aa
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-10-12 16:14:55 +0530

    Time: 78 ms (14.22%) | Memory: 59.8 MB (35.69%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,24 +1,21 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int[] ans = new int[nums.length - k+1];
         int n = nums.length;
-         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        
-        // initial window
-        for (int i = 0; i < k; i++) {
-            pq.offer(new int[]{nums[i], i});
+        int[] ans = new int[n - k+1];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[0]-a[0]);
+
+        for(int i=0; i<k; i++){
+            pq.add(new int[]{nums[i], i});
         }
         ans[0] = pq.peek()[0];
-        
-        for (int i = k; i < n; i++) {
-            pq.offer(new int[]{nums[i], i});
-            
-            // remove elements out of window
-            while (pq.peek()[1] <= i - k) {
+        for(int i=k; i<n; i++){
+            pq.add(new int[]{nums[i], i});
+
+            while(!pq.isEmpty() && pq.peek()[1] <= i-k){
                 pq.poll();
             }
-            
-            ans[i - k + 1] = pq.peek()[0];
+
+            ans[i-k+1] = pq.peek()[0];
         }
         return ans;
     }
