d7efed4d88eb575cccf4105ea5cbf638e2ab5e52
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-10-14 00:29:49 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (77.54%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,5 +1,9 @@
 class Solution {
+    static int[][] t = new int[23][23];
     public boolean predictTheWinner(int[] nums) {
+        for(int[] i: t){
+            Arrays.fill(i, -1);
+        }
         int sum = 0;
         for(int n: nums) sum+=n;
         int p1 = Winner(nums, 0, nums.length-1);
@@ -8,8 +12,9 @@ class Solution {
     }
     public static int Winner(int[] arr, int i, int j){
         if( i> j) return 0;
+        if(t[i][j] != -1) return t[i][j];
         int f = arr[i] + Math.min(Winner(arr, i+2, j), Winner(arr, i+1, j-1));
         int s = arr[j] + Math.min(Winner(arr, i+1, j-1), Winner(arr, i, j-2));
-        return Math.max(f,s);
+        return t[i][j] = Math.max(f,s);
     }
 }
