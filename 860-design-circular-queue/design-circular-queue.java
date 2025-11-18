19167919d75c2a8b1026cc56d455a57ec70fc12a
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-11-19 00:35:39 +0530

    Time: 5 ms (17.39%) | Memory: 46.7 MB (5.73%) - LeetSync

---
+++ 860-design-circular-queue/design-circular-queue.java
@@ -0,0 +1,51 @@
+class MyCircularQueue {
+
+    private int[] arr;
+    private int front;
+    private int rear;
+    private int size;
+    private int capacity;
+
+    public MyCircularQueue(int k) {
+        arr = new int[k];
+        capacity = k;
+        size = 0;
+        front = 0;
+        rear = -1;
+    }
+    
+    public boolean enQueue(int value) {
+        if (isFull()) return false;
+        
+        rear = (rear + 1) % capacity;
+        arr[rear] = value;
+        size++;
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if (isEmpty()) return false;
+
+        front = (front + 1) % capacity;
+        size--;
+        return true;
+    }
+    
+    public int Front() {
+        if (isEmpty()) return -1;
+        return arr[front];
+    }
+    
+    public int Rear() {
+        if (isEmpty()) return -1;
+        return arr[rear];
+    }
+    
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    public boolean isFull() {
+        return size == capacity;
+    }
+}
