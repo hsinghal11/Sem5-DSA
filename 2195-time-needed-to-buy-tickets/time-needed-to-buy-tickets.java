b2b381a4a94182525d75cd56783d8d43ccc0f03d
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-11-19 01:13:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (51.00%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -2,15 +2,18 @@ class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         int ans = 0;
         int n = tickets.length;
-        while(tickets[k] > 0){
-            for(int i=0; i<n; i++){
-                if(tickets[i] == 0){
-                    continue;
-                }
-                tickets[i]--;
-                ans++;
-                if(i ==k && tickets[i] == 0) return ans;
-            }
+        int moves = tickets[k];
+
+        // k se phele jo moves ke equal ya chota h vo khtm hoga
+
+        for(int i=0; i<=k; i++){
+            ans += Math.min(tickets[i], moves);
+        } 
+
+        // k ke baad jo moves -1 ye chota ya equal vo bhi khtm ho jayega to unko bhi add
+
+        for(int i=k+1; i<n; i++){
+            ans += Math.min(tickets[i], moves - 1); // moves hamesa >= 1 h isliye 1-1 = 0 hoga min not negative
         }
         return ans;
     }
