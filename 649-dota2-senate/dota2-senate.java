cabbcb8533d6a03a4356f02ae9d6c98caedca4c0
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-11-18 21:27:28 +0530

    Time: 10 ms (89.44%) | Memory: 47.2 MB (9.01%) - LeetSync

---
+++ 649-dota2-senate/dota2-senate.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        
+        Queue<Integer> qr = new LinkedList<>();
+        Queue<Integer> qd = new LinkedList<>();
+        int n = senate.length();
+
+        for(int i = 0; i < n; i++){
+            if(senate.charAt(i) == 'R')
+                qr.offer(i);
+            else
+                qd.offer(i);
+        }
+
+        while(!qr.isEmpty() && !qd.isEmpty()){
+            int ri = qr.poll();
+            int di = qd.poll();
+
+            if(ri < di) qr.offer(ri + n);
+            else qd.offer(di + n);
+        }
+        
+        return qd.size() == 0? "Radiant": "Dire";
+    }
+}
