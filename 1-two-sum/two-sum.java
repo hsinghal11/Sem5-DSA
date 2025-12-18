e53ca2cbc4436097fbfe6741e55aa30ef3e4504f
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-12-18 22:04:36 +0530

    Time: 2 ms (99.11%) | Memory: 47.3 MB (11.73%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,30 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        return sum(nums,target,nums.length);
-    }
-    public static int[] sum(int[] arr, int target, int n) {
-        int[] temp = arr.clone();
-        Arrays.sort(arr);
-        int i = 0;
-//        System.out.println(Arrays.toString(temp));
-//        System.out.println(Arrays.toString(arr));
-        int j = temp.length - 1;
-        while (i < j) {
-            if (arr[i] + arr[j] == target) {
-                break;
-            } else if (arr[i] + arr[j] < target) {
-                i++;
-            } else {
-                j--;
-            }
+    HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]))
+                return new int[] { i, map.get(target - nums[i]) };
+            map.put(nums[i], i);
         }
-        int[] ans = new int[2];
-        for (int k = 0; k < n; k++) {
-            if(temp[k] == arr[i]) ans[0]=k;
-        }
-        for (int k = 0; k < n; k++) {
-            if(temp[k]==arr[j] && k!=ans[0] ) ans[1]=k;
-        }
-        return ans;
+        return new int[] { 0, 0 };
     }
 }
