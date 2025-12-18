877cd7425b40dc852d191056aaac38e44a753d75
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2025-12-18 23:24:41 +0530

    Time: 8 ms (10.86%) | Memory: 102.7 MB (22.37%) - LeetSync

--- 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.java
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.java
@@ -1,35 +1,41 @@
 class Solution {
     public long maxProfit(int[] prices, int[] strategy, int k) {
-        long ans = Long.MIN_VALUE;
-        long profit = 0;
         int n = prices.length;
-        long[] pre = new long[n];
-        long[] pre1 = new long[n];
-        pre1[0] = prices[0];
-        for(int i=0; i<n; i++){
-            profit += prices[i]*strategy[i];
-            pre[i] = profit;
-        }
-        for(int i=1; i<n; i++){
-            pre1[i] = pre1[i-1]+prices[i];
-        }
-        ans = Math.max(ans, profit);
-        for(int time=0; time+k<=n; time++){
-            long profit_now = 0;
-            if(time-1 >= 0){
-                profit_now += pre[time-1];
-            }
 
-            int from = time + k / 2;
-            int to = time + k - 1;
-            profit_now += pre1[to] - (from > 0 ? pre1[from - 1] : 0);
-
-            if(time+k-1<n-1){
-                profit_now += pre[n-1] - pre[time+k-1];
-            }
-            ans = Math.max(ans,profit_now);
+        // original profit
+        long base = 0;
+        for (int i = 0; i < n; i++) {
+            base += (long) prices[i] * strategy[i];
         }
+
+        // prefix sum of original contributions
+        long[] pre = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            pre[i + 1] = pre[i] + (long) prices[i] * strategy[i];
+        }
+
+        // prefix sum of prices
+        long[] pricePre = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            pricePre[i + 1] = pricePre[i] + prices[i];
+        }
+
+        long ans = base;
+
+        for (int l = 0; l + k <= n; l++) {
+            int mid = l + k / 2;
+            int r = l + k;
+
+            // remove original window contribution
+            long remove = pre[r] - pre[l];
+
+            // add forced sell contribution (second half)
+            long add = pricePre[r] - pricePre[mid];
+
+            long curr = base - remove + add;
+            ans = Math.max(ans, curr);
+        }
+
         return ans;
-
     }
 }
