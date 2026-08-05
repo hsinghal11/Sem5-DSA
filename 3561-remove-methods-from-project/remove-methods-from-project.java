da2ca15168eeb16faca809a6635c1c537c1e4958
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2026-08-05 12:09:45 +0530

    Time: 20 ms (100.00%) | Memory: 280.2 MB (79.81%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.java
+++ 3561-remove-methods-from-project/remove-methods-from-project.java
@@ -1,61 +1,50 @@
 class Solution {
     public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
-        ArrayList<Integer>[] gp = new ArrayList[n];
-        boolean addAll = false;
-        List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < n; i++)
-            gp[i] = new ArrayList<>();
+        int m = invocations.length;
 
-        for (int[] d : invocations) {
-            int a = d[0];
-            int b = d[1];
-            gp[a].add(b);
-        }
+        // Build CSR adjacency list for forward edges (a -> b) and reverse edges (b -> a)
+        int[] outDeg = new int[n];
+        for (int[] e : invocations) outDeg[e[0]]++;
+        int[] head = new int[n + 1];
+        for (int i = 0; i < n; i++) head[i + 1] = head[i] + outDeg[i];
+        int[] adj = new int[m];
+        int[] pos = head.clone();
+        for (int[] e : invocations) adj[pos[e[0]]++] = e[1];
 
-        boolean[] inf = new boolean[n];
+        // Step 1: find all suspicious methods (k + everything reachable from k)
+        boolean[] suspicious = new boolean[n];
+        int[] queue = new int[n];
+        int qh = 0, qt = 0;
 
-        Queue<Integer> q = new LinkedList<>();
-        q.add(k);
-
-        while (!q.isEmpty()) {
-            int poll = q.poll();
-            if (inf[poll])
-                continue;
-
-            inf[poll] = true;
-
-            q.addAll(gp[poll]);
-        }
-        boolean[] vis = new boolean[n];
-        for (int nn = 0; nn < n; nn++) {
-            if (inf[nn] || vis[nn])
-                continue;
-
-            q.add(nn);
-            while (!q.isEmpty()) {
-                int poll = q.poll();
-                if (vis[poll])
-                    continue;
-                ans.add(poll);
-
-                vis[poll] = true;
-
-                for (int i : gp[poll]) {
-                    if (inf[i]) {
-                        addAll = true;
-                        break;
-                    } else {
-                        q.offer(i);
-                    }
+        suspicious[k] = true;
+        queue[qt++] = k;
+        while (qh < qt) {
+            int u = queue[qh++];
+            for (int i = head[u]; i < head[u + 1]; i++) {
+                int v = adj[i];
+                if (!suspicious[v]) {
+                    suspicious[v] = true;
+                    queue[qt++] = v;
                 }
             }
         }
-        if (addAll) {
-            ans = new ArrayList<>();
-            for (int i = 0; i < n; i++)
-                ans.add(i);
+
+        // Step 2: check if any NON-suspicious method invokes a suspicious method
+        boolean canRemove = true;
+        for (int[] e : invocations) {
+            int a = e[0], b = e[1];
+            if (!suspicious[a] && suspicious[b]) {
+                canRemove = false;
+                break;
+            }
         }
 
+        List<Integer> ans = new ArrayList<>();
+        if (canRemove) {
+            for (int i = 0; i < n; i++) if (!suspicious[i]) ans.add(i);
+        } else {
+            for (int i = 0; i < n; i++) ans.add(i);
+        }
         return ans;
     }
 }
