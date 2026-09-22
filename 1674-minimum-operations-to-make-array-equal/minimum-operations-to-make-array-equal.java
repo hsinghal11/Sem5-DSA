756e19e9cbb540a578ed0c85fc4d3f5e80c5dbb4
Himanshu Singhal <116331947+hsinghal11@users.noreply.github.com>
2026-09-22 23:08:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (89.42%) - LeetSync

--- 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.java
+++ 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.java
@@ -1,12 +1,8 @@
 class Solution {
     public int minOperations(int n) {
-        int mid = ((2*(n-1)/2) + 1 + (2* (n/2))+1)/2;
-        int ans = 0;
-        int t;
-        if((n & 1) != 1) ans++;
-        for(int i=1; i<mid-1; i+=2){
-            ans += (mid - i);
-        }
-        return ans;   
+        int mid = n / 2;
+        if (n % 2 == 0)
+            return mid * mid;
+        return mid * (mid + 1);  
     }
 }
